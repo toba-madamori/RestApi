3b90bab2ea0cd34b3bca8ca29deb0017fe6d65e6
toba-madamori <tobajamezmadamori@gmail.com>
2021-08-21 20:49:05 +0100

    refactored my function views introducing drf-status codes and the request-response object provided by the drf

--- app/views.py
+++ app/views.py
@@ -2,11 +2,12 @@ import re
 from django.shortcuts import render
 from .models import BlogModel
 from .serializers import BlogSerializer
-from rest_framework import serializers, views
+from rest_framework import serializers, views, status
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
 
 
@@ -15,33 +16,33 @@ def blog_home(request):
     if request.method == 'GET':
         articles = BlogModel.objects.all()
         serializer = BlogSerializer(articles, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
+
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = BlogSerializer(data=data)
+        serializer = BlogSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)    
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def blogpost(request, pk):
     try:
         snippet = BlogModel.objects.get(pk=pk)
     except BlogModel.DoesNotExist:
-        return HttpResponse(status = 404)
+        return Response(status = status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = BlogSerializer(snippet)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
+
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = BlogSerializer(snippet, data=data)
+        serializer = BlogSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'DELETE':
         snippet.delete()
-        return HttpResponse(status = 204)                            
+        return Response(status = status.HTTP_204_NO_CONTENT)                            
             
