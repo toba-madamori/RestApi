de8903b322efcdf69744fddf83f62ebf9653b0ad
toba-madamori <tobajamezmadamori@gmail.com>
2021-08-25 21:58:23 +0100

    refactored my views and started using class based views having Apiview as the parent class

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import blog_home, blogpost
+from .views import BlogListView, BlogDetailView
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('', blog_home, name='home'),
-    path('post-detail/<int:pk>', blogpost, name='post-detail'),
+    path('', BlogListView.as_view(), name='home'),
+    path('post-detail/<int:pk>', BlogDetailView.as_view(), name='post-detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- app/views.py
+++ app/views.py
@@ -1,48 +1,52 @@
-import re
+from django.http.response import Http404
 from django.shortcuts import render
 from .models import BlogModel
 from .serializers import BlogSerializer
-from rest_framework import serializers, views, status
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from rest_framework import  status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 # Create your views here.
 
 
-@api_view(['GET', 'POST'])
-def blog_home(request, format=None):
-    if request.method == 'GET':
+class BlogListView(APIView):
+    def get(self,request, format=None):
         articles = BlogModel.objects.all()
         serializer = BlogSerializer(articles, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = BlogSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def blogpost(request, pk, format=None):
-    try:
-        snippet = BlogModel.objects.get(pk=pk)
-    except BlogModel.DoesNotExist:
-        return Response(status = status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
+
+
+class BlogDetailView(APIView):
+    def get_object(self, pk):
+        try:
+            return BlogModel.objects.get(pk=pk)
+        except BlogModel.DoesNotExist:
+            raise Http404
+
+    def get(self,request, pk, format=None):
+        snippet = self.get_object(pk)
         serializer = BlogSerializer(snippet)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         serializer = BlogSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
+
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         snippet.delete()
         return Response(status = status.HTTP_204_NO_CONTENT)                            
-            
+                
