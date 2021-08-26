409e1d61e751d55565df4142e511e6a8691b01eb
toba-madamori <tobajamezmadamori@gmail.com>
2021-08-26 21:50:05 +0100

    refactored the code to work with the GenericApiView class and the mixins classes, together they provide powerful abstractions that help with the general CRUD functionalities, note that if you want to have more freedom to customize as you wish you should probably work with just the ApiView class or less

--- app/views.py
+++ app/views.py
@@ -6,47 +6,34 @@ from rest_framework import  status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import mixins
+from rest_framework import generics
 # Create your views here.
 
 
-class BlogListView(APIView):
-    def get(self,request, format=None):
-        articles = BlogModel.objects.all()
-        serializer = BlogSerializer(articles, many=True)
-        return Response(serializer.data)
+class BlogListView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = BlogModel.objects.all()
+    serializer_class = BlogSerializer
 
-    def post(self, request, format=None):
-        serializer = BlogSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
+    def get(self,request, *args, **kwargs):
+        return self.list(request, *args, *kwargs)
+        
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, *kwargs)
+        
 
 
 
+class BlogDetailView(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = BlogModel.objects.all()
+    serializer_class = BlogSerializer
 
-class BlogDetailView(APIView):
-    def get_object(self, pk):
-        try:
-            return BlogModel.objects.get(pk=pk)
-        except BlogModel.DoesNotExist:
-            raise Http404
+    def get(self,request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def get(self,request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = BlogSerializer(snippet)
-        return Response(serializer.data)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-    def put(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = BlogSerializer(snippet, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        snippet.delete()
-        return Response(status = status.HTTP_204_NO_CONTENT)                            
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)                            
                 
