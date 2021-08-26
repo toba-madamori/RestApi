366425b90af42aa11d3ae9546c013b515b590051
toba-madamori <tobajamezmadamori@gmail.com>
2021-08-26 21:54:49 +0100

    yet again I refactored the code using already mixed-in generic class based views, reducing our code and preventing the DRY concept

--- app/views.py
+++ app/views.py
@@ -11,29 +11,13 @@ from rest_framework import generics
 # Create your views here.
 
 
-class BlogListView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class BlogListView(generics.ListCreateAPIView):
     queryset = BlogModel.objects.all()
     serializer_class = BlogSerializer
 
-    def get(self,request, *args, **kwargs):
-        return self.list(request, *args, *kwargs)
-        
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, *kwargs)
-        
 
-
-
-class BlogDetailView(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+class BlogDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = BlogModel.objects.all()
     serializer_class = BlogSerializer
-
-    def get(self,request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)                            
+                     
                 
